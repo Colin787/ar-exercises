0fd7621e9324e33bd20616c5f44261ffa3b79e2a
Colin787 <colinpark4@gmail.com>
2017-07-04 22:13:51 +0000

    added first exercise

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,27 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+class Store < ActiveRecord::Base
+  has_many :employees
+  validates:name, :length => {:minimum => 3, message: "must be longer than 2 chars"}
+  validates:annual_revenue, :numericality => {:only_integer => true,
+    :greater_than => 0, message: 'must be integer value and between 30/200'}
+
+  burnaby = 
+  Store.create(name: 'Burnaby', annual_revenue: 300000, 
+  mens_apparel: true,
+  womens_apparel: true)  
+
+  richmond = 
+  Store.create(name: 'Richmond', annual_revenue: 1260000, 
+  mens_apparel: false,
+  womens_apparel: true)
+
+  gastown = 
+  Store.create(name: 'Gastown', annual_revenue: 190000, 
+  mens_apparel: true,
+  womens_apparel: false)
+end
+
+puts Store.count('id')
