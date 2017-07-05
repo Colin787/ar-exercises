16dd508ee6108cb161ca5533fabff70971cae32f
Colin787 <colinpark4@gmail.com>
2017-07-05 05:57:30 +0000

    added exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,28 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "WHat will you Store name be??"
+print "> "
+store_name = gets.chomp
+
+
+puts "Revenue?"
+print "> "
+store_revenue = gets.chomp
+
+
+puts "Mens Apparel true or false?"
+print "> "
+mens_apparel = gets.chomp
+
+puts "Womens Apparel true or false?"
+print "> "
+womens_apparel = gets.chomp
+
+newStore = Store.create(name: store_name, annual_revenue: store_revenue,
+  mens_apparel: mens_apparel,
+  womens_apparel: womens_apparel)
+
+p newStore.errors[:name]
+p newStore.errors[:annual_revenue]
