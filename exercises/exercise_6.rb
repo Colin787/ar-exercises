53fb48d81a27d09ebb60ab28b2dbad552a5dff6d
Colin787 <colinpark4@gmail.com>
2017-07-05 05:57:15 +0000

    added exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,26 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+  validates:first_name, presence: {message: 'cant be nothing'}
+  validates:last_name, presence: {message: 'cant be nothing'}
+  validates:hourly_rate, :numericality => {:only_integer => true,
+    :greater_than => 30, :less_than => 200, message: 'must be number between 30/200'}
+  validates:store_id, presence: {message: 'must belong to a store'}
+  
+
+end
+
+ @store1.employees.create(first_name: "Colin", 
+  last_name: "Park", 
+  hourly_rate: 60)
+
+@store2.employees.create(first_name: "Kaylee",
+  last_name: "Stuart",
+  hourly_rate: 35)
+
+@store2.employees.create(first_name: "Kay", 
+  last_name: "Park",
+  hourly_rate: 240)
