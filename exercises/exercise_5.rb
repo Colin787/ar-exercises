3547d8a5e7a694df8d8d8917cbe32fb6788dcba6
Colin787 <colinpark4@gmail.com>
2017-07-05 05:18:27 +0000

    added exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts 'Total rev is: ' + Store.sum('annual_revenue').to_s
+
+puts 'The average is: ' + Store.average('annual_revenue').to_s
+
+puts 'Stores makng bank!!  ' +  Store.where('annual_revenue > 1000000').count('id').to_s
